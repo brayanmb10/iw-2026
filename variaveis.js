c2dc7b3458d7ff40f7f00b3e4a562dabe3d92334
brayan <brayanmendesbarros@gmail.com>
2026-02-23 08:45:47 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+var nomeDaVariavel = "valor da variavel";
+var outravariavel = "valor da outra variavel";
+var variavelidade = 22;
+var variaveltenhocabelobranco = false;
+var variavelIndefinida ;
+var a = 14;
+var b = 42;
+var tr = (a == 42);
+var fl = (a == b);
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+
+console.log(nomeDaVariavel);
+console.log(outravariavel);
+console.log(variavelidade);
+console.log(variaveltenhocabelobranco);
+console.log(variavelIndefinida);
+
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
